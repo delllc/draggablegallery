7314fb34c0ca75d011c40091fbac6b4b700041f7
delllc <s.shahmatov2014@yandex.ru>
2022-05-20 18:48:10 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  
+  setTimeout(function(){
+    
+    document.body.classList.add('loaded')
+
+
+
+
+
+    Draggable.create('.gallery', {
+    bounds: 'body',
+    inertia: true
+  })
+})
+}
